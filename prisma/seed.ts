c9368eb5bbf1a372c3b92a080cebb411f672999b
rian-124 <afriyanlionel0@gmail.com>
2026-05-24 21:17:56 +0700

    feat: add husky prepush and precommit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
-import { seeder } from 'nestjs-seeder';
+
 import { DatabaseModule } from '../src/database/database.module';
 import { UserSeeder } from '../src/seeder/user.seeder';
+import { seeder } from 'nestjs-seeder';
 
 seeder({
   imports: [DatabaseModule],
